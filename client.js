6cf76a8502e6a793eda591c666776a3bcc2906a4
thzero <thzero@users.noreply.github.com>
2020-10-23 23:43:35 -0500

    updates

--- client.js
+++ client.js
@@ -26,6 +26,21 @@ class BaseClientGrpcService extends Service {
 		const config = this._config.getBackend(key);
 		return config.baseUrl;
 	}
+
+	async _host(correlationId, key) {
+		this._enforceNotNull('BaseClientGrpcService', '_host', key, 'key', correlationId);
+
+		let baseUrl = config.baseUrl;
+		if (config.discoverable) {
+			this._enforceNotNull('BaseClientGrpcService', '_host', config.disoveryName, 'disoveryName', correlationId);
+
+			baseUrl = this._serviceDiscoveryResources.getService(correlationId, config.disoveryName);
+		}
+		else
+			baseUrl = config.baseUrl;
+
+		return baseUrl;
+	}
 }
 
 export default BaseClientGrpcService;

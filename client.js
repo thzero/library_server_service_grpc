a713375710fa886a41c4a55d9246c7736d6cf9d3
thzero <thzero@users.noreply.github.com>
2020-10-27 20:16:01 -0500

    resource discovery updates

--- client.js
+++ client.js
@@ -1,8 +1,17 @@
 import * as grpc from '@grpc/grpc-js';
+import { Mutex as asyncMutex } from 'async-mutex';
 
 import Service from '@thzero/library_server/service/index';
 
 class BaseClientGrpcService extends Service {
+	constructor() {
+		super();
+
+		this._mutex = new asyncMutex();
+
+		this._baseUrls = new Map();
+	}
+
 	async _execute(correlationId, func, client, request) {
 		this._enforceNotNull('BaseClientGrpcService', '_execute', func, 'func', correlationId);
 
@@ -18,28 +27,46 @@ class BaseClientGrpcService extends Service {
 		});
 	}
 
-	get _credentials() {
-		return grpc.credentials.createInsecure();
-	}
-
-	_host(key) {
-		const config = this._config.getBackend(key);
-		return config.baseUrl;
-	}
-
-	async _host(correlationId, key) {
+	_host(correlationId, key) {
+		this._enforceNotNull('BaseClientGrpcService', '_host', key, 'key', correlationId);
 		this._enforceNotNull('BaseClientGrpcService', '_host', key, 'key', correlationId);
 
-		let baseUrl = config.baseUrl;
-		if (config.discoverable) {
-			this._enforceNotNull('BaseClientGrpcService', '_host', config.disoveryName, 'disoveryName', correlationId);
+		const config = this._config.getBackend(key);
+		this._enforceNotNull('BaseClientGrpcService', '_host', config, 'config', correlationId);
 
-			baseUrl = this._serviceDiscoveryResources.getService(correlationId, config.disoveryName);
+		let baseUrl = config.baseUrl;
+
+		if (config.discoverable) {
+			baseUrl = this._baseUrls.get(name);
+			if (!baseUrl)
+				return baseUrl;
+
+			const release = await this._mutex.acquire();
+			try {
+				let service = this._baseUrls.get(name);
+				if (!service)
+					return this._successResponse(service, correlationId);
+
+				this._enforceNotNull('AxiosRestCommunicationService', '_determineUrl', config.discoveryName, 'discoveryName', correlationId);
+
+				const result = this._serviceDiscoveryResources.getService(correlationId, config.discoveryName);
+				this._enforceNotNull('AxiosRestCommunicationService', '_determineUrl', result, 'result', correlationId);
+				this._enforceNotNull('AxiosRestCommunicationService', '_determineUrl', result.Address, 'result.Address', correlationId);
+				this._enforceNotNull('AxiosRestCommunicationService', '_determineUrl', result.Meta, 'result.Meta', correlationId);
+
+				baseUrl = `http${result.Meta.secure ? 's' : ''}s://${result.Address}${result.Port ? `:${result.Port}` : ''}` + cofnig.discoveryRoot;
+				this._baseUrls.set(name, baseUrl);
+			}
+			finally {
+				release();
+			}
 		}
-		else
-			baseUrl = config.baseUrl;
 
 		return baseUrl;
+	}
+
+	get _credentials() {
+		return grpc.credentials.createInsecure();
 	}
 }
 
